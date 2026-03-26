e0f28bad63e8856325a998d18a651cc64f63e90f
amberagnihotri <amberagnihotri0@gmail.com>
2026-03-26 15:18:56 +0100

    Newest version

--- TuneTrace/Controllers/DiscoverController.cs
+++ TuneTrace/Controllers/DiscoverController.cs
@@ -1,12 +1,46 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TuneTrace.Repositories;
 
 namespace TuneTrace.Controllers
 {
     public class DiscoverController : Controller
     {
+        private readonly ZoekRepository _zoekRepository;
+
+        public DiscoverController(IConfiguration configuration)
+        {
+            _zoekRepository = new ZoekRepository(configuration);
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Zoek(string zoekterm)
+        {
+            if (string.IsNullOrWhiteSpace(zoekterm) || zoekterm.Length < 2)
+            {
+                TempData["Fout"] = "Please enter at least 2 characters.";
+                return RedirectToAction("Index");
+            }
+
+            // Recente zoekopdrachten opslaan in sessie
+            var recente = HttpContext.Session.GetString("RecentSearchhistory");
+            var lijst = recente != null ? recente.Split(',').ToList() : new List<string>();
+            lijst.Remove(zoekterm);
+            lijst.Insert(0, zoekterm);
+            if (lijst.Count > 5) lijst = lijst.Take(5).ToList();
+            HttpContext.Session.SetString("RecentSearchhistory", string.Join(',', lijst));
+
+            ViewBag.Zoekterm = zoekterm;
+            ViewBag.Artiesten = _zoekRepository.ZoekArtiesten(zoekterm);
+            ViewBag.Albums = _zoekRepository.ZoekAlbums(zoekterm);
+            ViewBag.Songs = _zoekRepository.ZoekSongs(zoekterm);
+            ViewBag.RecenteZoekopdrachten = lijst;
+
+            return View("Index");
         }
     }
 }
